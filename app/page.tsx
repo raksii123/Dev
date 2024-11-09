068a2af74d0c12d0028b56e4b558014a7edeaaf5
raksii123 <rakshithghiremath028@gamil.com>
2024-11-09 00:05:22 -0800

    implementing routing and folder structure

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-zinc-800 flex-center">
-      <h1>hello</h1>
-    </div>
-  );
-}
