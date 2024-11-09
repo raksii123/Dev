5072915751a2673717ef90f9a3198bb5193e81d7
raksii123 <rakshithghiremath028@gamil.com>
2024-11-08 23:13:50 -0800

    "full config app with tailwind"

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-zinc-800 flex-center">
+      <h1>hello</h1>
+    </div>
+  );
+}
